3c58691debc03473264df71e361b51348c5b48f9
JoanChirinos <jchirinos@stuy.edu>
2018-01-23 10:47:48 -0500

    Started working on Grapher and CalcUtil

---
+++ Grapher.java
@@ -0,0 +1,33 @@
+/*
+       __                     ________    _      _
+      / /___  ____ _____     / ____/ /_  (_)____(_)___  ____  _____
+ __  / / __ \/ __ `/ __ \   / /   / __ \/ / ___/ / __ \/ __ \/ ___/
+/ /_/ / /_/ / /_/ / / / /  / /___/ / / / / /  / / / / / /_/ (__  )
+\____/\____/\__,_/_/ /_/   \____/_/ /_/_/_/  /_/_/ /_/\____/____/
+
+~Joan Chirinos, <DATE>
+*/
+
+/**********************************************************************
+
+ **********************************************************************/
+
+import java.util.ArrayList;
+import jutils.*;
+
+public class Grapher {
+
+  //Instance Vars
+
+  //data is { {xval, yval}, {xval, yval}, ... }
+  private ArrayList<ArrayList<Double>> _data;
+
+  public Grapher() {
+    _data = new ArrayList<ArrayList<Double>>();
+  }
+
+  public void graph(String equation) {
+
+  }
+
+}//end class
